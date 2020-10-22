196f02bc6c92e9f4f173775c4a8c675ffbb2fd7e
Alexis Mathiot <alexis.mathiot@gmail.com>
2020-10-22 09:06:20 +0200

    début responsive

--- assets/js/src/script.js
+++ assets/js/src/script.js
@@ -2,6 +2,8 @@ let txt = 'Bienvenue Sur Mon Portfolio'; /* The text */
 let speed = 50; /* The speed/duration of the effect in milliseconds */
 let i = 0;
 
+
+
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("typewritter").innerHTML += txt.charAt(i);
@@ -9,5 +11,6 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
 
 typeWriter();
