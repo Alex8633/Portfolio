c13a69f93d148531faf19754f4d9d52c9fee3208
Alexis Mathiot <alexis.mathiot@gmail.com>
2020-10-22 14:05:21 +0200

    nav responsive

--- assets/js/src/script.js
+++ assets/js/src/script.js
@@ -3,7 +3,6 @@ let speed = 50; /* The speed/duration of the effect in milliseconds */
 let i = 0;
 
 
-
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("typewritter").innerHTML += txt.charAt(i);
@@ -13,4 +12,24 @@ function typeWriter() {
 }
 
 
-typeWriter();
+let typewritter = document.getElementById("typewritter");
+if (typewritter) {
+  typeWriter();
+} 
+
+// menu responsive
+
+var navResp = document.getElementById("navAnim");
+var target = document.getElementById("navDeroule");
+
+navResp.addEventListener("click", function(e) {
+  target.classList.toggle('is-opened');
+});
+
+function myFunction() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
